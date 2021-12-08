1f88736086a32a9b90170cc5cbc39f2fd869edea
koladev <onaelmangabo@gmail.com>
2021-12-09 00:27:59 +0100

    - feat: add Discount model

--- discount/models.py
+++ discount/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Discount(models.Model):
+    code = models.CharField(max_length=35, unique=True, db_index=True)
+    value = models.FloatField()
+    description = models.TextField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
+    ended = models.DateTimeField()
