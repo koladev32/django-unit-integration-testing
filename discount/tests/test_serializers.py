215e19e62e2b8aebd257cc350bc38a78abc379a4
koladev <onaelmangabo@gmail.com>
2021-12-09 12:28:27 +0100

    - test: add serializer test serializer

--- discount/tests/test_serializers.py
+++ discount/tests/test_serializers.py
@@ -1,15 +1,18 @@
+import pytest
+
 from django.utils.timezone import now, timedelta
 from discount.serializers import DiscountSerializer
 
 
+@pytest.mark.django_db
 def test_valid_discount_serializer():
     valid_serializer_data = {
         "code": "DIS21",
         "value": 5,
         "description": "Some lines",
-        "ended": (now() + timedelta(days=2)).strftime()
+        "ended": (now() + timedelta(days=2))
     }
 
     serializer = DiscountSerializer(data=valid_serializer_data)
-    assert serializer.is_valid()
+    assert serializer.is_valid(raise_exception=True)
     assert serializer.validated_data == valid_serializer_data
