313065a6046efb862cf8333df1363a840e6cf40f
koladev <onaelmangabo@gmail.com>
2021-12-09 15:09:21 +0100

    - test: add more tests

--- discount/tests/test_viewset.py
+++ discount/tests/test_viewset.py
@@ -27,3 +27,37 @@ def test_add_discount(client):
 
     discounts = Discount.objects.all()
     assert discounts.count() == 1
+
+
+@pytest.mark.django_db
+def test_get_all_discounts(client):
+    response = client.get(
+        "/api/discount/",
+    )
+
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_retrieve_discount(client):
+
+    response = client.post(
+        "/api/discount/",
+        {
+            "code": "DIS21",
+            "value": 5,
+            "description": "Some lines",
+            "ended": (now() + timedelta(days=2))
+        },
+        content_type="application/json"
+    )
+
+    assert response.status_code == 201
+
+    response_data = response.data
+
+    response = client.get(
+        f"/api/discount/{response_data['id']}/",
+    )
+
+    assert response.status_code == 200
--- discount/viewsets.py
+++ discount/viewsets.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from discount.serializers import DiscountSerializer
+from discount.models import Discount
 
 
 class DiscountViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post']
     serializer_class = DiscountSerializer
     permission_classes = (AllowAny,)
+    queryset = Discount.objects.all()
