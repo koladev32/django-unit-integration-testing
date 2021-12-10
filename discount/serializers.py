3494614489bc4e2182319413b1f09a4c710fff33
koladev <onaelmangabo@gmail.com>
2021-12-10 14:59:24 +0100

    - feat: add apply_discount serializer

--- discount/serializers.py
+++ discount/serializers.py
@@ -1,4 +1,7 @@
+from django.utils.timezone import now
+
 from rest_framework import serializers
+from rest_framework import validators
 
 from discount.models import Discount
 
@@ -8,3 +11,19 @@ class DiscountSerializer(serializers.ModelSerializer):
         model = Discount
         fields = ('id', 'code', 'description', 'created', 'value', 'created', 'ended',)
         read_only_fields = ('id', 'created',)
+
+
+class ApplyDiscountSerializer(serializers.Serializer):
+    code = serializers.CharField(max_length=35)
+
+    def validate(self, attrs):
+        code = attrs.get('code')
+        try:
+            discount = Discount.objects.get(code=code)
+        except Discount.DoesNotExist:
+            raise validators.ValidationError("This discount doesn't exist.")
+
+        if discount.ended < now():
+            raise validators.ValidationError("This discount has expired.")
+
+        return attrs
--- discount/tests/test_serializers.py
+++ discount/tests/test_serializers.py
@@ -1,7 +1,8 @@
 import pytest
 
 from django.utils.timezone import now, timedelta
-from discount.serializers import DiscountSerializer
+from discount.serializers import DiscountSerializer, ApplyDiscountSerializer
+from discount.models import Discount
 
 
 @pytest.mark.django_db
@@ -16,3 +17,43 @@ def test_valid_discount_serializer():
     serializer = DiscountSerializer(data=valid_serializer_data)
     assert serializer.is_valid(raise_exception=True)
     assert serializer.validated_data == valid_serializer_data
+
+
+@pytest.mark.django_db
+def test_apply_discount_serializer():
+    # CREATING THE DISCOUNT
+    discount = Discount(code="DIS20", value=5, description="Some discount",
+                        created=now(), ended=now() + timedelta(days=2))
+    discount.save()
+
+    discount_data = {
+        "code": "DIS20"
+    }
+
+    serializer = ApplyDiscountSerializer(data=discount_data)
+    assert serializer.is_valid(raise_exception=True)
+
+
+@pytest.mark.django_db
+def test_apply_expired_discount_serializer():
+    # CREATING THE DISCOUNT
+    discount = Discount(code="DIS19", value=5, description="Some discount",
+                        created=now(), ended=now() - timedelta(days=2))
+    discount.save()
+
+    discount_data = {
+        "code": "DIS19"
+    }
+
+    serializer = ApplyDiscountSerializer(data=discount_data)
+    assert not serializer.is_valid()
+
+
+@pytest.mark.django_db
+def test_apply_non_exist_discount_serializer():
+    discount_data = {
+        "code": "DIS32"
+    }
+
+    serializer = ApplyDiscountSerializer(data=discount_data)
+    assert not serializer.is_valid()
