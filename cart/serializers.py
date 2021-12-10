9be8687637241c30c297ba50624414acb69fbbf2
koladev <onaelmangabo@gmail.com>
2021-12-10 14:09:45 +0100

    - feat: add cart serializer

---
+++ cart/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from cart.models import Cart
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ('id', 'total', 'total_discounted', 'amount_discounted', 'items_number', 'created', 'currency',)
+        read_only_fields = ('id', 'created',)
