5e9c41d6b52e415d0510f3cf4f8bc1d6d8134b0a
koladev <onaelmangabo@gmail.com>
2021-12-10 15:04:33 +0100

    - test: add apply_discount viewset test

--- cart/tests/test_viewsets.py
+++ cart/tests/test_viewsets.py
@@ -1,6 +1,7 @@
 import pytest
-
+from django.utils.timezone import now, timedelta
 from cart.models import Cart
+from discount.models import Discount
 
 
 @pytest.mark.django_db
@@ -53,3 +54,38 @@ def test_retrieve_cart(client):
     )
 
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_apply_discount_to_cart(client):
+    # CREATING THE DISCOUNT
+    discount = Discount(code="DIS20", value=5, description="Some discount",
+                        ended=now() + timedelta(days=2))
+    discount.save()
+
+    response = client.post(
+        "/api/cart/",
+        {
+            "total": 20,
+            "currency": "USD",
+            "items_number": 5
+        },
+        content_type="application/json"
+    )
+    assert response.status_code == 201
+
+    response_data = response.data
+
+    response = client.post(
+        f"/api/cart/{response_data['id']}/apply_discount/",
+        {
+            "code": "DIS20"
+        },
+        content_type="application/json"
+    )
+
+    assert response.status_code == 200
+    assert response.data['total'] == 15
+    assert response.data['total_discounted'] == 15
+    assert response.data['amount_discounted'] == 5
+
