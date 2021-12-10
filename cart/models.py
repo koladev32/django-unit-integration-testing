5f5f6cbdb82c9bab57fc8ccda5bc647b40e62980
koladev <onaelmangabo@gmail.com>
2021-12-10 14:45:10 +0100

    - feat: add apply_discount method

--- cart/models.py
+++ cart/models.py
@@ -8,3 +8,14 @@ class Cart(models.Model):
     total_discounted = models.FloatField(default=0)
     amount_discounted = models.FloatField(default=0)
     created = models.DateTimeField(auto_now_add=True)
+
+    def apply_discount(self, discount):
+
+        self.amount_discounted += discount.value
+        self.total_discounted = self.total - self.amount_discounted
+
+        self.total = self.total_discounted
+
+        self.save(update_fields=['total', 'amount_discounted', 'total_discounted'])
+
+
