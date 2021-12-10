a245d1b9688255fb6c558afd5ae3da2ab6ed6902
koladev <onaelmangabo@gmail.com>
2021-12-10 14:38:17 +0100

    - test: add apply_discount method

--- cart/tests/test_models.py
+++ cart/tests/test_models.py
@@ -1,6 +1,8 @@
 import pytest
+from django.utils.timezone import now, timedelta
 
 from cart.models import Cart
+from discount.models import Discount
 
 
 @pytest.mark.django_db
@@ -11,3 +13,20 @@ def test_cart_model():
     assert cart.total == 10
     assert cart.currency == "USD"
     assert cart.items_number == 5
+
+
+@pytest.mark.django_db
+def test_apply_discount_to_cart():
+    cart = Cart(total=20, currency="USD", items_number=5)
+    cart.save()
+
+    # Creating the discount
+    discount = Discount(code="DIS20", value=5, description="Some discount",
+                        ended=now() + timedelta(days=2))
+    discount.save()
+
+    cart.apply_discount(discount)
+
+    assert cart.amount_discounted == 5
+    assert cart.total == 15
+    assert cart.total_discounted == 15
