a6cafe13009b1e30066497e86b21b8f934f26c2c
koladev <onaelmangabo@gmail.com>
2021-12-10 14:28:06 +0100

    - feat: registering viewsets

--- routers.py
+++ routers.py
@@ -1,9 +1,11 @@
 from rest_framework import routers
 
 from discount.viewsets import DiscountViewSet
+from cart.viewsets import CartViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'discount', DiscountViewSet, basename='discount')
+router.register(r'cart', CartViewSet, basename='cart')
 
 urlpatterns = router.urls
